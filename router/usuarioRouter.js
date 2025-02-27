81aac1b8d1eeb6810d1fea5329f26196c595b8b3
wesly <saritabenavides880@gmail.com>
2025-02-27 18:29:45 -0500

    segundo intento

--- router/usuarioRouter.js
+++ router/usuarioRouter.js
@@ -8,3 +8,5 @@ router.put('/user/:id', UsuarioController.actualizarUsuario);
 router.delete('/user/:id', UsuarioController.eliminarUsuario);
 router.get('/user/:correo', UsuarioController.buscarUsuario);
 module.exports=router;
+
+//hola esto esta echo por wess
